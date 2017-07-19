9161480fa41664f37bdcafe0a5ce0065df093dfe
Tej Chajed <tchajed@mit.edu>
2017-07-19 10:00:28 +0100

    Initial parsing of placeholders

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,87 @@
+{-# LANGUAGE Rank2Types, FlexibleContexts #-}
 module Lib
-    ( someFunc
+    (
+        ParserT
+
+        -- syntax
+      , Conjugation(..)
+      , Name(..)
+      , Type(..)
+      , Placeholder(..)
+
+        -- parsers
+      , conjugation
+      , identifier
+      , typeP
+      , placeholder
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Parsec
+
+type ParserT a = forall m s. Stream s m Char => ParsecT s () m a
+
+comment :: ParserT ()
+comment = char '#' >> manyTill anyChar (try (char '\n')) >> return ()
+
+data Conjugation =
+    Past
+    | Participle
+    | Infinitive
+    deriving (Eq, Show)
+
+data Name = Name String
+    deriving (Eq, Show)
+
+data Type =
+    Verb Conjugation
+    | Noun
+    | Preposition
+    | Reference Name
+    | OneOf [String]
+    deriving (Eq, Show)
+
+data Placeholder =
+    PlainType Type
+    | Binding Name Type
+    deriving (Eq, Show)
+
+conjugation :: ParserT Conjugation
+conjugation = choice [
+      try (string "past") >> return Past
+    , try (string "participle") >> return Participle
+    , try (string "infinitive") >> return Infinitive
+    ] <?> "conjugation"
+
+identifier :: ParserT Name
+identifier = Name <$> do
+    c <- letter
+    cs <- many (alphaNum <|> char '_')
+    return (c:cs)
+
+functionCall :: String -> ParserT a -> ParserT a
+functionCall name p =
+    string name >>
+    between (char '(') (char ')') p
+
+typeP :: ParserT Type
+typeP = choice [
+        string "noun" >> return Noun
+        , Verb <$> functionCall "verb" conjugation
+        , string "preposition" >> return Preposition
+        , char '?' >> Reference <$> identifier
+        , OneOf <$> functionCall "oneof"
+            (literal `sepBy` (char ','))
+    ]
+    where
+        literal :: ParserT String
+        literal = many (noneOf ",)")
+
+placeholder :: ParserT Placeholder
+placeholder = between (char '{') (char '}')
+    (try binderPlaceholder <|> typePlaceholder)
+    where binderPlaceholder = do
+            b <- identifier
+            char ':'
+            t <- typeP
+            return $ Binding b t
+          typePlaceholder = PlainType <$> typeP
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,74 @@
+{-# LANGUAGE Rank2Types, FlexibleContexts #-}
+import Lib
+import Test.Hspec
+import Text.Parsec
+import Control.Monad.Identity (Identity)
+
+-- infix version of expectParsesTo
+shouldParseTo :: (Stream s Identity Char, Show a, Eq a) => (ParsecT s () Identity a, s) -> a -> Expectation
+shouldParseTo (p, s) x = runParser p () "" s `shouldBe` (Right x)
+
+shouldNotParse :: (Show a, Eq a) => ParserT a -> String -> Expectation
+shouldNotParse p s = (runParser p () "" s) `shouldSatisfy`
+    (\r -> case r of
+            Left _ -> True
+            Right _ -> False)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Parser" $ do
+        describe "conjugation" $ do
+            it "past conjugation should parse" $ do
+                (conjugation, "past") `shouldParseTo` Past
+            it "participle conjugation should parse" $ do
+                (conjugation, "participle") `shouldParseTo` Participle
+            it "invalid conjugation should not parse" $ do
+                conjugation `shouldNotParse` "foobar"
+        describe "type" $ do
+            it "noun type should parse" $ do
+                (typeP, "noun") `shouldParseTo` Noun
+            it "verb type should parse" $ do
+                (typeP, "verb(past)") `shouldParseTo` Verb Past
+            it "verb type should parse with infinitive" $ do
+                (typeP, "verb(infinitive)") `shouldParseTo` Verb Infinitive
+            it "one of should parse with one value" $ do
+                (typeP, "oneof(a)") `shouldParseTo` OneOf ["a"]
+            it "one of should parse with two values" $ do
+                (typeP, "oneof(a,b)") `shouldParseTo` OneOf ["a", "b"]
+            it "one of should capture whitespace" $ do
+                (typeP, "oneof(a ,b, c)") `shouldParseTo` OneOf ["a ", "b", " c"]
+            it "preposition type should parse" $ do
+                (typeP, "preposition") `shouldParseTo` Preposition
+            it "reference type should parse" $ do
+                (typeP, "?foobar") `shouldParseTo` Reference (Name "foobar")
+        describe "identifier" $ do
+            it "should parse alphabetic identifiers" $ do
+                (identifier, "foo") `shouldParseTo` Name "foo"
+            it "should parse alpha followed by digits" $ do
+                (identifier, "a1") `shouldParseTo` Name "a1"
+            it "should allow _" $ do
+                (identifier, "a_binding_name") `shouldParseTo` Name "a_binding_name"
+            it "should not allow empty" $ do
+                identifier `shouldNotParse` ""
+            it "should not allow initial digit" $ do
+                identifier `shouldNotParse` "123name"
+        describe "placeholder" $ do
+            it "should parse unbound noun" $ do
+                (placeholder, "{noun}") `shouldParseTo`
+                    (PlainType Noun)
+            it "should parse bound noun" $ do
+                (placeholder, "{name:noun}") `shouldParseTo`
+                    (Binding (Name "name") Noun)
+            it "should parse bound verb" $ do
+                (placeholder, "{a1:verb(past)}") `shouldParseTo`
+                    (Binding (Name "a1") (Verb Past))
+            it "preposition function call should not parse" $ do
+                typeP `shouldNotParse` "{preposition()}"
+            it "should parse references" $ do
+                (placeholder, "{?name}") `shouldParseTo` PlainType (Reference (Name "name"))
+            it "should parse bound oneof" $ do
+                (placeholder, "{sel:oneof(a,b,c)}") `shouldParseTo`
+                    Binding (Name "sel") (OneOf ["a", "b", "c"])
+            it "should parse bound reference" $ do
+                (placeholder, "{name2:?name}") `shouldParseTo`
+                    Binding (Name "name2") (Reference (Name "name"))
